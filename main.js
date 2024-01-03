ab6b83d39a3c9937c70effcaab53224f66b82298
Yosepo <josephaliaga08@gmail.com>
2024-01-03 18:24:46 -0500

    Agregando contenido a JavaScript

---
+++ main.js
@@ -0,0 +1,16 @@
+let colorActual = "brown"
+
+for (let i = 0; i < 196; i++) {
+    let bloque = document.createElement("div")
+    bloque.className = "bloque"
+    document.getElementById("contenedor").appendChild(bloque)
+    bloque.onclick = cambiar
+}
+
+function colors (color) {
+    colorActual = color
+}
+
+function cambiar (n) {
+    n.target.style.backgroundColor = colorActual
+}
